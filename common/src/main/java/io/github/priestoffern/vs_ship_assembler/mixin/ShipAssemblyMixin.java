cc9c150c5df67c4475691b6f66cfff2bda3312b0
SlimeyAr <lostbhijaya@gmail.com>
2024-04-14 20:08:49 +0700

    Progress on mixin into ShipAssemblyKt

--- common/src/main/java/io/github/priestoffern/vs_ship_assembler/mixin/ShipAssemblyMixin.java
+++ common/src/main/java/io/github/priestoffern/vs_ship_assembler/mixin/ShipAssemblyMixin.java
@@ -5,10 +5,11 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
-import org.joml.Vector3ic;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import io.github.priestoffern.vs_ship_assembler.VsShipAssemblerTags;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import org.valkyrienskies.core.api.ships.ServerShip;
 import org.valkyrienskies.core.util.datastructures.DenseBlockPosSet;
 import org.valkyrienskies.mod.common.assembly.ShipAssemblyKt;
@@ -17,16 +18,17 @@ import java.util.Collection;
 import java.util.List;
 
 @Mixin(ShipAssemblyKt.class)
-public class ShipAssemblyMixin {
+public final class ShipAssemblyMixin {
 
     // Overwrite the createNewShipWithBlocks function with our own function which wont break redstone stuff and cause duplication glitch
-    public static final ServerShip createNewShipWithBlocks(@NotNull BlockPos centerBlock, @NotNull DenseBlockPosSet blocks, @NotNull ServerLevel level) {
+    @Inject(method = "createNewShipWithBlocks", at = @At("HEAD"), cancellable = true)
+    private static void onCreateNewShipWithBlocks(@NotNull BlockPos centerBlock, @NotNull DenseBlockPosSet blocks, @NotNull ServerLevel level, CallbackInfoReturnable<ServerShip> cir) {
         Collection<BlockPos > blockList = null;
         blocks.forEach(vec -> {
             if (!level.getBlockState(new BlockPos(vec.x(), vec.y(), vec.z())).getTags().anyMatch(tag -> tag.equals(VsShipAssemblerTags.FORBIDDEN_ASSEMBLE))) {
                 blockList.add(new BlockPos(vec.x(), vec.y(), vec.z()));
             }
         });
-        return (ServerShip) PhysicUtility.INSTANCE.assembleToContraption((Level) level, (List<BlockPos>) blockList, true, 1.0);
+        cir.setReturnValue((ServerShip) PhysicUtility.INSTANCE.assembleToContraption((Level) level, (List<BlockPos>) blockList, true, 1.0));
     }
 }
