86e0ff4ffabc6af5f926ef72dd248b95a47a697d
SlimeyAr <lostbhijaya@gmail.com>
2024-04-20 22:14:49 +0700

    Add logger (willl use minecraft logger later)

--- common/src/main/kotlin/io/github/priestoffern/vs_ship_assembler/VsShipAssemblerMod.kt
+++ common/src/main/kotlin/io/github/priestoffern/vs_ship_assembler/VsShipAssemblerMod.kt
@@ -1,7 +1,10 @@
 package io.github.priestoffern.vs_ship_assembler
 
+import java.util.logging.Logger
+
 object VsShipAssemblerMod {
     const val MOD_ID = "vs_ship_assembler"
+    val LOGGER = Logger.getLogger("VsShipAssembler")
 
     @JvmStatic
     fun init() {
--- common/src/main/kotlin/io/github/priestoffern/vs_ship_assembler/rendering/Renderer.kt
+++ common/src/main/kotlin/io/github/priestoffern/vs_ship_assembler/rendering/Renderer.kt
@@ -1,13 +1,5 @@
 package io.github.priestoffern.vs_ship_assembler.rendering
 
-import com.google.common.base.Supplier
-import com.mojang.blaze3d.vertex.PoseStack
-
-import net.minecraft.client.Camera
-import net.minecraft.client.Minecraft
-
-import org.valkyrienskies.mod.common.shipObjectWorld
-
 object Renderer {
     var CurrentId:Long = 0;
     val toRender = mutableListOf<RenderingData>()
