bc9b2ac5a7a551917260956d3c656aad755b5d89
SlimeyAr <lostbhijaya@gmail.com>
2024-05-24 22:49:17 +0700

    Some helpful constant

---
+++ common/src/main/kotlin/io/github/slimeymc/koml/Double.kt
@@ -0,0 +1,9 @@
+package io.github.slimeymc.koml
+
+const val EPSILON = 1e-6
+const val SQRT12 = 0.7071067811865476
+const val SQRT2 = 1.4142135623730951
+const val LN2 = 0.6931471805599453
+const val TAU = 6.283185307179586
+const val PI = 3.141592653589793
+const val E = 2.718281828459045
